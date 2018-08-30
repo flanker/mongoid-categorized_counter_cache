98d45fbca0cb1a38522d747dbd99ead1fb9e1a27
FENG Zhichao <flankerfc@gmail.com>
2018-08-30 12:03:11 +0800

    Initialize empty gem

---
+++ lib/mongoid/categorized_counter_cache.rb
@@ -0,0 +1,7 @@
+require 'mongoid/categorized_counter_cache/version'
+
+module Mongoid
+  module CategorizedCounterCache
+    # Your code goes here...
+  end
+end
---
+++ lib/mongoid/categorized_counter_cache/version.rb
@@ -0,0 +1,5 @@
+module Mongoid
+  module CategorizedCounterCache
+    VERSION = '0.1.0'
+  end
+end
