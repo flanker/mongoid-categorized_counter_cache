1a73a84c1880e97f7fc87d630ffac0d506567b90
FENG Zhichao <flankerfc@gmail.com>
2018-10-26 10:07:58 +0800

    Use a field name instead of a block to determine the category.
    This is bacause when the `category` is updated, we need to decrease the original
    category counter and increase the current counter. But if we are using
    block to determine the category, it is hard to find out the original one.
    So use a field name instead. (field original valud is easy to get)

--- spec/app/models/staff.rb
+++ spec/app/models/staff.rb
@@ -5,8 +5,6 @@ class Staff
   field :gender    # male, female
 
   belongs_to :company, counter_cache: true
-  categorized_counter_cache :company do |staff|
-    staff.gender.to_s
-  end
+  categorized_counter_cache :company, by: :gender
 
 end
